876358b25f62b6d902d5867bbb77d27d4bba01cd
Oswaldo Teran <oswaldo_217@hotmail.com>
2017-01-20 09:08:25 -0400

    limpiando archivos basura y comentarios

--- index.php
+++ index.php
@@ -4,20 +4,13 @@
 	require_once  'controller/changeLanguage.php';
 	require_once  'controller/sendEmail.php';
 ?>
-<html lang="en">
+<html lang="es">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Web Progress</title>
 <meta name="description" content="">
 <meta name="author" content="">
-
-<!-- Favicons
-    ================================================== -->
-<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
-<link rel="apple-touch-icon" href="img/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon"  href="img/apple-touch-icon-114x114.png">
 
 <!-- Bootstrap -->
 <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
@@ -29,26 +22,17 @@
 <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
 <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="js/modernizr.custom.js"></script>
 
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
 <!-- Navigation
     ==========================================-->
 <nav id="menu" class="navbar navbar-default navbar-fixed-top">
   <div class="container"> 
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
       <a class="navbar-brand" href="#home">Web Progress</a> </div>
       
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#home" class="page-scroll"><?php echo $inicio ?></a></li>
@@ -65,9 +49,7 @@
         </li>	
       </ul>
     </div>
-    <!-- /.navbar-collapse --> 
   </div>
-  <!-- /.container-fluid --> 
 </nav>
 
 <!-- Header -->
@@ -159,7 +141,7 @@
 </div>
 <!-- Portfolio Section -->
 <div id="works-section">
-  <div class="container"> <!-- Container -->
+  <div class="container">
     <div class="section-title">
       <h2><?php echo $nuestroPortafolio ?></h2>
       <hr>
@@ -331,16 +313,11 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
 <script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
 <script type="text/javascript" src="js/bootstrap.js"></script> 
 <script type="text/javascript" src="js/SmoothScroll.js"></script> 
 <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script> 
 <script type="text/javascript" src="js/jquery.isotope.js"></script> 
 <script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
-
-<!--<script type="text/javascript" src="js/contact_me.js"></script>--> 
-
-<!--<script type="text/javascript" src="js/contact_me.js"></script> -->
 
 <!-- Javascripts
     ================================================== --> 
