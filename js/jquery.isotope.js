77a622eb3ac1a3a8dfda42a358aac9a8e6274081
migmosquera <migmosquera@hotmail.com>
2017-01-20 09:56:29 -0400

    “subiendo_cambio_en_el_index_js”

--- js/jquery.isotope.js
+++ js/jquery.isotope.js
@@ -1,32 +1,17 @@
-/**
- * Isotope v1.5.25
- * An exquisite jQuery plugin for magical layouts
- * http://isotope.metafizzy.co
- *
- * Commercial use requires one-time license fee
- * http://metafizzy.co/#licenses
- *
- * Copyright 2012 David DeSandro / Metafizzy
- */
 
-/*jshint asi: true, browser: true, curly: true, eqeqeq: true, forin: false, immed: false, newcap: true, noempty: true, strict: true, undef: true */
-/*global jQuery: false */
 
 (function( window, $, undefined ){
 
   'use strict';
 
-  // get global vars
+ 
   var document = window.document;
   var Modernizr = window.Modernizr;
 
-  // helper function
   var capitalize = function( str ) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
-  // ========================= getStyleProperty by kangax ===============================
-  // http://perfectionkills.com/feature-testing-css-properties/
 
   var prefixes = 'Moz Webkit O Ms'.split(' ');
 
@@ -34,15 +19,15 @@
     var style = document.documentElement.style,
         prefixed;
 
-    // test standard property first
+    
     if ( typeof style[propName] === 'string' ) {
       return propName;
     }
 
-    // capitalize
+    
     propName = capitalize( propName );
 
-    // test vendor specific properties
+  
     for ( var i=0, len = prefixes.length; i < len; i++ ) {
       prefixed = prefixes[i] + propName;
       if ( typeof style[ prefixed ] === 'string' ) {
@@ -55,26 +40,7 @@
       transitionProp = getStyleProperty('transitionProperty');
 
 
-  // ========================= miniModernizr ===============================
-  // <3<3<3 and thanks to Faruk and Paul for doing the heavy lifting
-
-  /*!
-   * Modernizr v1.6ish: miniModernizr for Isotope
-   * http://www.modernizr.com
-   *
-   * Developed by:
-   * - Faruk Ates  http://farukat.es/
-   * - Paul Irish  http://paulirish.com/
-   *
-   * Copyright (c) 2009-2010
-   * Dual-licensed under the BSD or MIT licenses.
-   * http://www.modernizr.com/license/
-   */
-
-  /*
-   * This version whittles down the script just to check support for
-   * CSS transitions, transforms, and 3D transforms.
-  */
+  
 
   var tests = {
     csstransforms: function() {
@@ -83,7 +49,7 @@
 
     csstransforms3d: function() {
       var test = !!getStyleProperty('perspective');
-      // double check for Chrome's false positive
+     
       if ( test ) {
         var vendorCSSPrefixes = ' -o- -moz- -ms- -webkit- -khtml- '.split(' '),
             mediaQuery = '@media (' + vendorCSSPrefixes.join('transform-3d),(') + 'modernizr)',
@@ -107,15 +73,15 @@
   var testName;
 
   if ( Modernizr ) {
-    // if there's a previous Modernzir, check if there are necessary tests
+   
     for ( testName in tests) {
       if ( !Modernizr.hasOwnProperty( testName ) ) {
-        // if test hasn't been run, use addTest to run it
+      
         Modernizr.addTest( testName, tests[ testName ] );
       }
     }
   } else {
-    // or create new mini Modernizr that just has the 3 tests
+    
     Modernizr = window.Modernizr = {
       _version : '1.6ish: miniModernizr for Isotope'
     };
@@ -123,14 +89,14 @@
     var classes = ' ';
     var result;
 
-    // Run through tests
+
     for ( testName in tests) {
       result = tests[ testName ]();
       Modernizr[ testName ] = result;
       classes += ' ' + ( result ?  '' : 'no-' ) + testName;
     }
 
-    // Add the new classes to the <html> element.
+
     $('html').addClass( classes );
   }
 
