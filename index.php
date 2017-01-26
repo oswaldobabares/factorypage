56ff431817e5eea9cb746e5fe5fb6b429595a9a2
Oswaldo Teran <oswaldo_217@hotmail.com>
2017-01-26 16:49:57 -0400

    subiendo icono y eliminando el anterior

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
   <div class="container"> 
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      <img src="img/iconos/wp_icons.png" /> </div>
+      <img src="img/iconos/iconsWp.png" /> </div>
       
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
